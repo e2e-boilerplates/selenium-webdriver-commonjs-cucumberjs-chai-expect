b122503d13317a9cfc19f7066d69ce984215e3b6
Girma Nigusse <xgirma@gmail.com>
2019-10-21 23:21:09 -0400

    init

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,34 +1,31 @@
 const { Given, When, Then, AfterAll } = require("cucumber");
-const { Builder, By, Capabilities, Key } = require("selenium-webdriver");
+const { Builder, By, Capabilities, Key, until } = require("selenium-webdriver");
 const { expect } = require("chai");
 
 require("chromedriver");
 
 const capabilities = Capabilities.chrome();
 capabilities.set("chromeOptions", { w3c: false });
-const driver = new Builder().withCapabilities(capabilities).build();
+const browser = new Builder().withCapabilities(capabilities).build();
 
 Given("I am on the Google search page", async () => {
-  await driver.get("https://www.google.com");
+  await browser.get("https://www.google.com");
 });
 
 When("I search for {string}", async searchWord => {
-  const element = await driver.findElement(By.name("q"));
+  const element = await browser.findElement(By.name("q"));
   element.sendKeys(searchWord, Key.RETURN);
   element.submit();
 });
 
-Then(
-  "the page title should start with {string}",
-  { timeout: 60 * 1000 },
-  async searchWord => {
-    const title = await driver.getTitle();
-    const isTitleStartWithCheese =
-      title.toLowerCase().lastIndexOf(`${searchWord}`, 0) === 0;
-    expect(isTitleStartWithCheese).to.equal(true);
-  }
-);
+Then("the page title should start with {string}", async searchWord => {
+  await browser.wait(until.urlContains("search"), 5000);
+
+  const title = await browser.getTitle();
+  const words = title.split(" ");
+  expect(words[0]).to.equal(searchWord);
+});
 
 AfterAll("end", async () => {
-  await driver.quit();
+  await browser.quit();
 });
